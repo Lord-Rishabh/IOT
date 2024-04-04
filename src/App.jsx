fef9d6e890d339c3117babe94f2b8aa83da6a742
Rishabh <rishabhptl2003@gmail.com>
2024-04-04 19:35:09 +0530

    website alter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-// App.js
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css'; // Import your CSS file for global styles
+import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
@@ -20,7 +18,13 @@ function App() {
       }
     };
 
-    fetchData();
+    fetchData(); // Fetch data immediately when component mounts
+
+    // Setup interval to fetch data every 15 seconds
+    const interval = setInterval(fetchData, 15000);
+
+    // Cleanup function to clear interval when component unmounts or changes
+    return () => clearInterval(interval);
   }, []);
 
   if (!data) {
@@ -30,17 +34,17 @@ function App() {
   const { channel, feeds } = data;
 
   return (
-    <div className=" text-white">
-      <div className="container mx-auto px-4 py-8 ">
+    <div className="text-white">
+      <div className="container mx-auto px-4 py-8">
         <h1 className="text-2xl font-bold mb-4">{channel.name}</h1>
-        <div className="">
+        <div>
           {feeds.map(feed => (
-            <div key={feed.entry_id} className="bg-gray-800 p-4 rounded-lg">
+            <div key={feed.entry_id} className="bg-gray-800 p-4 rounded-lg mb-4">
               <h2 className="text-lg font-semibold mb-2">Entry ID: {feed.entry_id}</h2>
               <p><span className="font-semibold">Temperature:</span> {feed.field1} °C</p>
               <p><span className="font-semibold">Humidity:</span> {feed.field2}%</p>
               <p><span className="font-semibold">Light:</span> {feed.field3}</p>
-              <p><span className="font-semibold">Gas:</span> {feed.field4 == 1 ? "No" : "Yes"}</p>
+              <p><span className="font-semibold">Gas:</span> {feed.field4 === "1" ? "No" : "Yes"}</p>
               <p><span className="font-semibold">Created At:</span> {feed.created_at}</p>
             </div>
           ))}
